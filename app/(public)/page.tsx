9330a580e7cece7d648498dae5d281a47cf25e52
Joaquin <joakoperaltacorbellini@gmail.com>
2026-06-11 09:52:00 -0300

    creación de la estructura de carpetas

---
+++ app/(public)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="flex flex-1 flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Cabañas Ermitazh</h1>
+    </section>
+  );
+}
